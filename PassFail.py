81a863193328fcefc2088e07178809b79b41a776
Ketema Harris <ketema@midnightoilconsulting.com>
2019-08-24 06:36:00 -0400

    Commented the code a bit

--- PassFail.py
+++ PassFail.py
@@ -1,4 +1,6 @@
+#!/opt/local/bin/python
 
+#This function reads in the grades from a fil
 def readListOfGrades():
     gradeList = open("Prob01.in.txt", "r")
     numGrades = gradeList.readline()
@@ -8,7 +10,7 @@ def readListOfGrades():
 
     gradeList.close()
 
-
+#This Function determines if a grade passes or fails and print to the screen
 def determinePassFail( grade ):
     if int(grade) >= 70:
         print("PASS")
@@ -16,4 +18,5 @@ def determinePassFail( grade ):
 
     print("FAIL")
 
+#how we run the program
 readListOfGrades()
